dd073ae765a2ba71ce25968f8e7586a1e6403753
Akashdeep Nandi <adncorwin@gmail.com>
2021-03-17 16:47:52 +0530

    completed second task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [box, setBox] = useState<any>(null);
+  const [lastBox, setLastBox] = useState<any>(null);
+  const handleClick = () => {
+    try {
+      const num = Number(box);
+      if (lastBox) {
+        const el = document.getElementById(lastBox.toString());
+        // @ts-ignore
+        el.style.backgroundColor = "white";
+      }
+      if (!isNaN(num)) {
+        const el = document.getElementById(num.toString());
+        // @ts-ignore
+        el.style.backgroundColor = "red";
+        setLastBox(box);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setBox(e.target.value)}
+            />
+            <button
+              className="btn btn-primary"
+              style={{
+                borderRadius: "25px",
+                marginTop: "5rem",
+              }}
+              onClick={handleClick}
+            >
+              Color me
+            </button>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="row">
+            <div className="col" id="1">
+              1
+            </div>
+            <div className="col" id="2">
+              2
+            </div>
+            <div className="col" id="3">
+              3
+            </div>
+          </div>
+          <div className="row">
+            <div className="col" id="4">
+              4
+            </div>
+            <div className="col" id="5">
+              5
+            </div>
+            <div className="col" id="6">
+              6
+            </div>
+          </div>
+          <div className="row">
+            <div className="col" id="7">
+              7
+            </div>
+            <div className="col" id="8">
+              8
+            </div>
+            <div className="col" id="9">
+              9
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
